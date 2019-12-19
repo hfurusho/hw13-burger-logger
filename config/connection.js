5c03b9808bb4cc95368bff4aa457030b38ba59dc
Kami <51175640+hfurusho@users.noreply.github.com>
2019-12-19 13:03:26 -0800

    Moved env file to root and fixed the file path.

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql");
-require("dotenv").config({ path: "./.env" });
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
 const connection = mysql.createConnection({
   host: "localhost",
