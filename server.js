0c224f082b92eb77d23317b9b62147751d0c719c
Kami <51175640+hfurusho@users.noreply.github.com>
2019-12-19 13:20:52 -0800

    Set up server to use handlebars and routes.

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers/burgers_controller");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set up Handlebars.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at http://localhost:" + PORT);
+});
