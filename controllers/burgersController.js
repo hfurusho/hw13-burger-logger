61633597acf08c922994d993486af86c10bbc703
Kami <51175640+hfurusho@users.noreply.github.com>
2020-01-16 13:07:36 -0800

    Converted all routes to async functions.

---
+++ controllers/burgersController.js
@@ -0,0 +1,43 @@
+let express = require("express");
+let burger = require("../models/burger.js");
+
+let router = express.Router();
+
+router.get("/", async function(req, res) {
+  try {
+    console.log("in get /");
+    let data = await burger.selectAllBurgers();
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post("/api/burgers", async function(req, res) {
+  try {
+    let result = await burger.insertOneBurger(req.body.burgerName);
+    res.json({ id: result.insertId });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.put("/api/burgers/:id", async function(req, res) {
+  try {
+    const id = req.params.id;
+
+    let results = await burger.updateOneBurger("devoured", true, "id", id);
+    if (results.changedRows === 0) {
+      return res.status(404).end();
+    }
+    return res.status(200).end();
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+module.exports = router;
