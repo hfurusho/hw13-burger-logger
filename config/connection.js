7652a988bb59f78bf6d1823e4ad64197c578862d
Kami <51175640+hfurusho@users.noreply.github.com>
2020-01-17 12:55:32 -0800

    Set up sql connection for jawsdb.

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 const mysql = require("mysql");
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+let connection;
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  port: 3306,
-  password: process.env.BURGERSDB_PW,
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    port: 3306,
+    password: process.env.BURGERSDB_PW,
+    database: "burgers_db"
+  });
+}
 
 connection.connect(err => {
   if (err) throw err;
